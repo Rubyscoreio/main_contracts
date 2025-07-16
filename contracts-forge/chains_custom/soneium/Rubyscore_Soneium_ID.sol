383dd8c3e356580591ffa6f97b6e5968f1832371
rubyscore-tech <rubyscore_tech@lansoft.dev>
2025-07-16 12:05:53 +0300

    created id contract with custom name for soneium

---
+++ contracts-forge/chains_custom/soneium/Rubyscore_Soneium_ID.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import {RubyscoreID} from "contracts-forge/base/RubyscoreID.sol";
+
+contract Rubyscore_Soneium_ID is RubyscoreID {
+    constructor() {}
+}
