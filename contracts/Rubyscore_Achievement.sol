64f18fbe0049bc8ba9401d53da8d95c1be4f5ece
rubyscore-tech <rubyscore_tech@lansoft.dev>
2025-09-29 10:48:38 +0300

    fixed versions

--- contracts/Rubyscore_Achievement.sol
+++ contracts/Rubyscore_Achievement.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.21;
+pragma solidity ^0.8.21;
 
 import {IRubyscore_Achievement} from "./interfaces/IRubyscore_Achievement.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
--- contracts/interfaces/IRubyscore_Achievement.sol
+++ contracts/interfaces/IRubyscore_Achievement.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.21;
+pragma solidity ^0.8.21;
 
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
